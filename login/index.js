dd19a03d2e9334900661a2e1a07679432f0ec7d3
tonny <wodexintiao@gmail.com>
2015-04-03 15:26:03 +0800

    完成和server通讯封装（备尝艰辛）

--- login/index.js
+++ login/index.js
@@ -1,56 +1,26 @@
 !function(){
-	var Buffer = require('buffer').Buffer;
-	var URL = require('url');
-	var zlib = require('zlib');
-	var KEY = new Buffer('Crackers and the thief will suffer misfortune');
-	function encrypt(str){
-		str = new Buffer(str);
-		var len_key = KEY.length;
-		var result = '';
-		var pos = 0;
-		for(var i = 0, j = str.length; i<j; i++){
-			var code = str[i] ^ KEY[pos];
-			result += String.fromCharCode(code);
-			if(++pos == len_key){
-				pos = 0;
-			}
-		}
-		return result;
-	}
-	function request(url, params, callback){
-		var arr_url = URL.parse(url);
-		var data_send = encrypt(JSON.stringify(params));
-		var req = require('http').request({
-			hostname: arr_url.hostname,
-			port: 80,
-			method: 'POST',
-			path: arr_url.pathname,
-		}, function(res){
-			var chunks =[];
-			res.on('data', function(chunk){
-				chunks.push(chunk);
-			}).on('end', function(){
-				var buffer = Buffer.concat(chunks);
-				var data = encrypt(buffer, true);
-				// console.log(data);
-				if(!data){
-					callback && callback(new Error('no data'));
-				}else{
-					callback && callback(null, JSON.parse(data));
-				}
-				
-			});
-		}).on('error', function(e){
-			callback && callback(e);
-		});
-		req.write(data_send);
-		req.end();
-	}
+	var storage = localStorage;
 	function login(username, pwd, callback){
-		request('http://u.weather.com.cn/mobile/login', {
-			userName: username,
-			password: pwd
-		}, callback);
+		getUId(function(e, uid){
+			request('http://u.weather.com.cn/mobile/login', {
+				userName: username,
+				password: pwd
+			}, callback);
+		})
+	}
+	function getUId(callback){
+		var uid = storage.getItem('uid');
+		if(uid){
+			callback && callback(null, uid);
+		}else{
+			request('http://app.weather.com.cn/smartWeather', {"method":"uuid"}, function(e, data){
+				console.log(e, data);
+				if(!e){
+					storage.setItem('uid', data);
+				}
+				callback && callback(e, data);
+			});
+		}
 	}
 	function afterLogin(){
 		location.replace('../index.html');
@@ -66,7 +36,7 @@
 		if(!pwd){
 			return alert('密码不能为空！');
 		}
-		if(localStorage.getItem('auto')){
+		if(storage.getItem('login_info')){
 			afterLogin();
 		}else{
 			login(username, pwd, function(e, data){
@@ -75,9 +45,11 @@
 				}else{
 					var result = data.result;
 					if(result && result.status == 'SUCCESS' && result.data.groupId.indexOf(83) > -1){
-						localStorage.setItem('login_info', {
-							name: result.data.userName
-						});
+						storage.setItem('login_info', JSON.stringify({
+							name: result.data.userName,
+							uid: result.data.uId,
+							userId: storage.getItem('uid')
+						}));
 						afterLogin();
 					}else{
 						alert('用户名不存在或密码错误！');
@@ -85,7 +57,7 @@
 				}
 			});
 		}
-	}).click();
+	});
 	$(document).on('keypress', function(e){
 		if(e.keyCode == 13){
 			$btn_login.click();
---
+++ login/p_setting.js
@@ -0,0 +1,98 @@
+!function(){
+	var U = Util;
+	var os = require('os'),
+		fs = require('fs'),
+		path = require('path');
+    var tmpPath = os.tmpdir() || os.tmpDir || global.window.nwDispatcher.requireNwGui().App.dataPath;
+    tmpPath = path.join(tmpPath, '.pdf');
+    var os_info = os.platform() + ','+os.type();
+    var manifest = require('nw.gui').App.manifest;
+    var soft_info = manifest.name+','+manifest.version;
+	var _command_old = U.command;
+	var userInfo = {};
+	try{
+		userInfo = JSON.parse(localStorage.getItem('login_info'));
+	}catch(e){}
+	var fn_obj = {
+		'alert': function(msg){
+			alert(msg.msg);	
+		},
+		'getLoginName': function(p, callback){
+			callback && callback(userInfo.name || '');
+		},
+		'getCacheSize': function(p, callback){
+			fs.readdir(tmpPath, function(e, files){
+				if(!e){
+					var size_total = 0;
+					files.forEach(function(file){
+						var f_path = path.join(tmpPath, file);
+						size_total += fs.statSync(f_path).size;
+					});
+					size_total /= 1024;
+					if(size_total < 512){
+						size_total = size_total.toFixed(2) + 'K';
+					}else{
+						size_total /= 1024;
+						if(size_total < 512){
+							size_total = (size_total.toFixed(2))+'M';
+						}else{
+							size_total /= 1024;
+							size_total = (size_total.toFixed(2))+'G';
+						}
+					}
+				}
+				callback && callback(e?0: size_total);
+			});
+		},
+		'clearCache': function(p, callback){
+			fs.readdir(tmpPath, function(e, files){
+				if(!e){
+					files.forEach(function(file) {
+				        var fullName = path.join(tmpPath, file);
+				        if (fs.statSync(fullName).isDirectory()) {
+				        }else{
+				        	fs.unlinkSync(fullName);
+				        }
+				    });
+				}
+				callback && callback();
+			});
+		},
+		'logout': function(p, callback){
+			localStorage.removeItem('login_info');
+			location.replace('./login/index.html');
+		},
+		'feedback': function(p, callback){
+			p.userId = userInfo.userId;
+			p.osVersion = os_info;
+			p.softVersion = soft_info;
+			request('http://app.weather.com.cn/second/feedback/upload', {
+				appKey: '6f688d62594549a2',
+				data: p
+			}, function(e, data){
+				var return_data = {};
+				if(e){
+					return_data.e = e;
+				}else{
+					if(!data || data.status !== 'SUCCESS'){
+						return_data.e = new Error('出现错误, 请联系管理员！');
+					}
+				}
+				callback && callback(return_data);
+			});
+		}
+	};
+	U.command = function(fnName, param, callback){
+		var args = [].slice.call(arguments);
+		if(args.length == 2 && typeof param == 'function'){
+			callback = param;
+			param = null;
+		}
+		var fn = fn_obj[fnName];
+		if(fn){
+			fn(param, function(){
+				callback && callback.apply(null, arguments);
+			});
+		}
+	}
+}();
