c503e64a329f3ed703606d4c1ebba317fa1fa506
tonny <wodexintiao@gmail.com>
2016-05-17 16:52:15 +0800

    delete some files

--- shell/build.sh
+++ shell/build.sh
@@ -1,7 +1,7 @@
 SHELLPATH=$(cd `dirname $0`; pwd)
 PROJECT_PATH=$(cd $SHELLPATH; cd ..; pwd)
 TARGET_PATH=$PROJECT_PATH/target
-SOURCE_64=$TARGET_PATH/64
+# SOURCE_64=$TARGET_PATH/64
 SOURCE_32=$TARGET_PATH/32
 SOURCE_PATH=$TARGET_PATH/source
 #1. get source
@@ -16,13 +16,13 @@ node $SHELLPATH/change.js $SOURCE_PATH
 # mv $SOURCE_PATH/updater/node_modules $SOURCE_PATH/node_modules
 
 #3. 64 or 32
-rm -rf $SOURCE_64
+# rm -rf $SOURCE_64
 rm -rf $SOURCE_32
-mkdir -p $SOURCE_64
+# mkdir -p $SOURCE_64
 mkdir -p $SOURCE_32
 
-cp -R $SOURCE_PATH/* $SOURCE_64/
+# cp -R $SOURCE_PATH/* $SOURCE_64/
 cp -R $SOURCE_PATH/* $SOURCE_32/
 
-cp -R $PROJECT_PATH/libs/node-webkit-v0.11.2-win-x64/* $SOURCE_64
+# cp -R $PROJECT_PATH/libs/node-webkit-v0.11.2-win-x64/* $SOURCE_64
 cp -R $PROJECT_PATH/libs/node-webkit-v0.12.0-win-x32/* $SOURCE_32
